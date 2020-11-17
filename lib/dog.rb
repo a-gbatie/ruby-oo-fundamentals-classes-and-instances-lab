233e6e227e817fe0f94cf6912dfd35e67b55ef9e
a-gbatie <ange.batie@gmail.com>
2020-11-17 13:58:03 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+    def fluffy
+        puts "So soft!"
+    end
+end
+
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    def greeting
+        puts "Nice to meet you!"
+    end    
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
